5abd5c2cfa9ab5203782bfd27689a41048486152
Your Name <kbb25t@gmail.com>
2022-06-05 18:41:54 -0300

    Estrutura base

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const Quiz());
+}
+
+class Quiz extends StatelessWidget {
+  const Quiz({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Quiz App',
+      debugShowCheckedModeBanner: false,
+      home: MainMenu(),
+    );
+  }
+}
